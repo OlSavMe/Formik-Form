e6b266aae826581d8dbcf08712e96b95f19924a8
olsavHH <olga.savka@myy.haaga-helia.fi>
2020-02-06 23:23:57 +0200

    corrected error messages

--- src/components/Formikk.js
+++ src/components/Formikk.js
@@ -1,66 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-
-const Formikk = () => {
-    return (
-        <div>
-            <h1>Sign up form</h1>
-            <Formik
-                initialValues={{ firstName: '', lastName: '', email: '', password: '', confirmPassword: '' }}
-                validationSchema={SignupSchema}
-                onSubmit={values => { console.log(values) }}
-
-            >
-                {({ values, errors, touched }) => (
-                    <Form>
-                        <div >
-                            <Field name="firtsName" placeholder="First Name"></Field>
-                            {errors.firstName && touched.firstName ? (<div className="errors">{errors.firstName}</div>) : null}
-                        </div>
-                        <div className="errors">
-                            <Field name="lastName" placeholder="Last Name"></Field>
-                        </div>
-                        <div className="errors">
-                            <Field name="email" type="email" placeholder="Email"></Field>
-                        </div>
-                        <div >
-                            <Field name="password" type="password" placeholder="Password"></Field>
-                            {errors.password && touched.password ? (<div className="errors">{errors.password}</div>) : null}
-                        </div>
-                        <div className="errors">
-                            <Field name="confirmPassword" type="password" placeholder="Confirm Password"></Field>
-                        </div>
-                        <button type="submit">Submit</button>
-                        <pre>{JSON.stringify(values, null, 2)}</pre>
-
-                    </Form>
-                )
-
-                }
+//import { Form, Input, Title, Button } from './Theme.js';
 
 
 
+class Formikk extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Sign Up form</h1>
+                <Formik
+                    initialValues={{
+                        firstName: "",
+                        lastName: "",
+                        email: "",
+                        password: "",
+                        confirmPassword: ""
+                    }}
+                    validationSchema={SignupSchema}
+                    onSubmit={values => {
+                        //run function here, submit data to db
+                        console.log(values);
+                    }}
+                >
+                    {({ values, errors, touched }) => (
+                        <Form>
+                            <div>
+                                <Field name="firstName" placeholder="First Name" />
+                                {errors.firstName && touched.firstName ? (
+                                    <div className="errors">{errors.firstName}</div>
+                                ) : null}
+                            </div>
+                            <div>
+                                <Field name="lastName" placeholder="Last Name" />
+                                {errors.lastName && touched.lastName ? (
+                                    <div className="errors">{errors.lastName}</div>
+                                ) : null}
+                            </div>
+                            <div>
+                                <Field name="email" type="email" placeholder="Email" />
+                                {errors.email && touched.email ? (
+                                    <div className="errors">{errors.email}</div>
+                                ) : null}
+                            </div>
+                            <div>
+                                <Field name="password" type="password" placeholder="password" />
+                                {errors.password && touched.password ? (
+                                    <div className="errors">{errors.password}</div>
+                                ) : null}
+                            </div>
+                            <div>
+                                <Field
+                                    name="confirmPassword"
+                                    type="password"
+                                    placeholder="confirmPassword"
+                                />
+                                {errors.confirmPassword && touched.confirmPassword ? (
+                                    <div className="errors">{errors.confirmPassword}</div>
+                                ) : null}
+                            </div>
+                            <button type="submit">Submit</button>
+                            <pre>{JSON.stringify(values, null, 2)}</pre>
+                        </Form>
+                    )}
+                </Formik>
+            </div >
+        );
+    }
 
-            </Formik>
-        </div >
-    );
 }
 
 const SignupSchema = Yup.object().shape(
     {
         firstName: Yup.string()
-            .min(2, "Too short")
-            .max(50, "Too Long"),
-        // .required("Required"),
+            .min(2, "Too short!")
+            .max(50, "Too long")
+            .required("Required"),
+        lastName: Yup.string()
+            .min(2, "Too short!")
+            .max(50, "Too long!")
+            .required("Required"),
         email: Yup.string()
             .email("Invalid email")
             .required("required"),
         password: Yup.string()
-            .min(4, "minmum 4 chars")
-            .max(50, "too lomg")
+            .min(4, "minmum 4 symbols")
+            .max(50, "too long")
             .required("required"),
-
+        confirmPassword: Yup.string().oneOf(
+            [Yup.ref("password"), null],
+            "Password must be the same!"
+        )
     });
 
 export default Formikk;
